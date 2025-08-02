2d998f1a00ef3cb75ead3749ecc1d912163e4eab
Aamer2005 <2023bit064@sggs.ac.in>
2025-08-02 12:04:42 +0530

    star pattern

---
+++ pattern.py
@@ -0,0 +1,17 @@
+# size = int(input("Enter Size : "))
+
+# for line in range(1,size+1):
+#     for star in range(1,line+1):
+#         print("*",end="")
+#     print("")
+
+
+# for i in range(3) : 
+#     pass
+
+# print("Hello World")
+
+import sys
+
+val = "pqrs"
+print(sys.getrefcount(val))
